6567f5ff75125c16c3a299303735f5eb347b14c8
Isma Code <111458908+Oscar-RNS@users.noreply.github.com>
2023-08-24 22:09:35 -0600

    Modificación dirección de URL

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -27,45 +27,50 @@
                     aria-current="page">Home</a>
             </li>
             <li>
-                <a href="https://angular.io/"
+                <a href="https://angular.oscar-nava.mx/"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Angular</a>
             </li>
             <li>
-                <a href="https://es.legacy.reactjs.org/"
+                <a href="https://react.oscar-nava.mx/"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">React</a>
             </li>
             <li>
-                <a href="https://vuejs.org/"
+                <a href="https://vue.oscar-nava.mx/"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Vue</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://nodejs.org/es/about"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Node
                     JS</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://laravel.com/docs/10.x"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Laravel</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://www.yiiframework.com/doc/guide/2.0/es"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Yii
                     2</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://www.oracle.com/mx/java/technologies/java-ee-glance.html"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Java
                     EE</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://www.python.org/"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Phyton</a>
             </li>
             <li>
-                <a href="#"
+                <a href="https://dotnet.microsoft.com/es-es/"
+                    class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">.NET</a>
+            </li>
+            <li>
+                <a href="https://powerbi.microsoft.com/es-mx/"
                     class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Power
                     BI</a>
             </li>
+
         </ul>
     </div>
 </div>
